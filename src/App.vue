4c7e5739de94fdb9f1ccbd6c5cf288f890d33ed2
moHaHa <diab.4869@gmail.com>
2023-03-20 19:37:22 +0200

    feat : card component

--- src/App.vue
+++ src/App.vue
@@ -7,38 +7,6 @@
   <div bg-white p-xl>
     <TheCard />
   </div>
-  <div my-lg />
-  <div bg-white p-xl flex>
-    <TheCard />
-  </div>
-  <div my-lg />
-  <div bg-white p-xl flex>
-    <TheCard />
-    <TheCard />
-    <TheCard />
-  </div>
-  <div bg-white p-xl flex>
-    <div me-1>
-      <TheCard />
-    </div>
-    <div me-1>
-      <TheCard />
-    </div>
-    <div me-1>
-      <TheCard />
-    </div>
-  </div>
-  <div bg-white p-xl flex>
-    <div flex-1 px-1>
-      <TheCard />
-    </div>
-    <div flex-1 px-1>
-      <TheCard />
-    </div>
-    <div flex-1 px-1>
-      <TheCard />
-    </div>
-  </div>
   <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
     <RouterView />
     <TheFooter />
--- src/components/TheCard.vue
+++ src/components/TheCard.vue
@@ -2,14 +2,14 @@
 </script>
 
 <template>
-  <div block bg-card rounded-33px min-w-284px min-h-340px px-lg>
+  <div inline-block bg-card rounded-33px min-w-284px min-h-340px px-lg>
     <div flex fc pt-50px pb-30px>
       <div text-43px i-carbon-sun color-black />
     </div>
     <div mx-auto text-center txt-blue text-21px pb-30px font-bold>
       Vision
     </div>
-    <div max-w-284px mx-auto text-center txt-black head text-2xl text-16px>
+    <div max-w-284px mx-auto text-center txt-black un-break-words :style="{ 'lineHeight': '27px', 'word-break': 'break-all' }">
       Based on the strong principles of our Founders, we aim to be a leading diversified group operating world-wide
     </div>
   </div>
