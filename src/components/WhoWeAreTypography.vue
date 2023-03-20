bcbb1a377b4e155e631919239811bb886c32ae86
moHaHa <diab.4869@gmail.com>
2023-03-20 23:53:42 +0200

    feat : typography

---
+++ src/components/WhoWeAreTypography.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <div txt-blue text-29px mb-5px relative font-bold>
+      <h2 mb-8px>
+        WHO WE ARE
+      </h2>
+      <span block w-100px h-3px rounded-full ms-3px bg="line" />
+    </div>
+    <div leading-33px max-w-400px>
+      Al Modon Al Arabia Holding provides an
+      umbrella for a diversified range of
+      self-innovated, forward thinking and sustainable
+      group of companies integrating services that cater
+      to its clients and partners’ needs
+    </div>
+    <div flex fi mt-10px>
+      <div text-12px font-bold>
+        Read More
+      </div> <div ms-20px text-16px txt-line font-bold i-carbon-arrow-right />
+    </div>
+  </div>
+</template>
