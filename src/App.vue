8e45e688173aa8b9be44dbf1e9365ea27cb69d71
moHaHa <diab.4869@gmail.com>
2023-03-21 05:21:02 +0200

    chore : clean up

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,8 @@
 <template>
   <HeroLayout />
   <WhiteSection />
-
-  <div bg-white p-xl>
-    <TheCard />
-  </div>
-  <div bg-white p-xl>
-    <TheImageCard />
-  </div>
+  <CoolSection />
   <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
-    <RouterView />
     <TheFooter />
   </main>
 </template>
