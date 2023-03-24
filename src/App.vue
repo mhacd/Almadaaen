4065194bd2d96f7b7f864dce3503dfb44436e366
moHaHa <diab.4869@gmail.com>
2023-03-24 16:08:33 +0200

    feat : add aos

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+import AOS from 'aos'
 const intro = ref(true)
+onMounted(() => {
+  AOS.init()
+})
 // setTimeout(() => {
 //   intro.value = false
 // }, 2800)
@@ -12,7 +16,17 @@ const intro = ref(true)
     <WhiteSection />
     <CoolSection />
     <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
-      <div text-center>
+      <div>
+        <button
+          btn data-aos="fade-zoom-in"
+          data-aos-easing="ease-in-back"
+        >
+          aos
+        </button>
+      </div>
+      <div
+        text-center
+      >
         {{ $route.fullPath }}
       </div>
       <TheFooter />
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from 'virtual:generated-pages'
 import App from './App.vue'
-
+import 'aos/dist/aos.css'
 import '@unocss/reset/tailwind.css'
 import './styles/main.css'
 import 'uno.css'
